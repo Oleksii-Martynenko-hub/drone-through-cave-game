1dabd9f74e5be4d15a7817d93f14dc0057f799a0
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-10-21 12:13:48 +0300

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="drone-through-cave-game" />
+    </StyledApp>
+  );
+}
+
+export default App;
