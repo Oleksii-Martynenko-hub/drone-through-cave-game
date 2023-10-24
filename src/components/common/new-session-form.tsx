1799a8cd3424b346ad07af3d299917da37a05ee7
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-10-24 11:49:18 +0300

    Add form for new game session

---
+++ src/components/common/new-session-form.tsx
@@ -0,0 +1,106 @@
+import { ChangeEvent, useState, FormEvent } from 'react';
+import styled from 'styled-components';
+
+import Button from './button';
+
+export type NewSessionData = {
+  name: string;
+  difficulty: number;
+};
+
+interface Props {
+  onSubmit: (newSessionData: NewSessionData) => void;
+}
+
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  max-width: 320px;
+  margin-bottom: 40px;
+`;
+
+const Input = styled.input`
+  font-size: 18px;
+  line-height: 24px;
+  border: none;
+  padding: 10px 18px 10px;
+  border-radius: 6px;
+
+  flex: 100%;
+  margin-bottom: 10px;
+`;
+
+const InputRangeWrapper = styled.div`
+  display: grid;
+  grid-template: 40px 1fr / 1fr 32px;
+
+  width: 100%;
+  color: white;
+  font-size: 26px;
+  margin-bottom: 16px;
+
+  input {
+    grid-column: 1 / 3;
+  }
+`;
+
+const PlayButton = styled(Button)`
+  flex: 1 1 auto;
+`;
+
+function NewSessionForm({ onSubmit }: Props) {
+  const [name, setName] = useState('');
+  const [difficulty, setDifficulty] = useState(0);
+
+  function onSubmitForm(e: FormEvent) {
+    e.preventDefault();
+
+    onSubmit({ name, difficulty });
+    setName('');
+    setDifficulty(0);
+  }
+
+  function onChangePlayerName(e: ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+
+  function onChangeDifficulty(e: ChangeEvent<HTMLInputElement>) {
+    setDifficulty(+e.target.value);
+  }
+
+  return (
+    <Form onSubmit={onSubmitForm}>
+      <Input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Player name"
+        value={name}
+        onChange={onChangePlayerName}
+      />
+
+      <InputRangeWrapper>
+        <label htmlFor="difficulty">Difficulty level:</label>
+
+        <span>{difficulty}</span>
+
+        <input
+          type="range"
+          name="difficulty"
+          id="difficulty"
+          min={0}
+          max={10}
+          value={difficulty}
+          placeholder="Diff"
+          onChange={onChangeDifficulty}
+        />
+      </InputRangeWrapper>
+
+      <PlayButton>Play</PlayButton>
+    </Form>
+  );
+}
+
+export default NewSessionForm;
--- src/components/start-dialog.tsx
+++ src/components/start-dialog.tsx
@@ -1,24 +1,31 @@
 import { PropsWithChildren, useState } from 'react';
 import { Dialog } from '@headlessui/react';
+import styled from 'styled-components';
 
-import Button from './common/button';
+import NewSessionForm, { NewSessionData } from './common/new-session-form';
+
+const DialogContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function StartDialog({ children }: PropsWithChildren) {
   const [isOpen, setIsOpen] = useState(true);
 
-  function closeModal() {
+  function closeModal(newSessionData: NewSessionData) {
     setIsOpen(false);
   }
 
-  function openModal() {
-    setIsOpen(true);
-  }
-
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-      <Button onClick={closeModal}>Play</Button>
+    <Dialog open={isOpen} onClose={() => void 0}>
+      <Dialog.Panel>
+        <DialogContentWrapper>
+          <NewSessionForm onSubmit={closeModal} />
 
-      {children}
+          {children}
+        </DialogContentWrapper>
+      </Dialog.Panel>
     </Dialog>
   );
 }
