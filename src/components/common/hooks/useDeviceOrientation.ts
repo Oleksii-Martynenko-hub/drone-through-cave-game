64f944fa7fbe519b883ee65d1015ea872c4f8de6
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-11-06 18:47:52 +0200

    Add origin offset as started orientation

--- src/components/common/hooks/useDeviceOrientation.ts
+++ src/components/common/hooks/useDeviceOrientation.ts
@@ -1,23 +1,30 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 
 import { WithoutNull } from 'src/types/common';
+
+type Orientation = WithoutNull<Omit<DeviceMotionEventAcceleration, 'z'>> | null;
 
 export const useDeviceOrientation = (
   setIsShowModal: Dispatch<SetStateAction<boolean>>,
 ) => {
-  const [orientationOrigin, setOrientationOrigin] =
-    useState<WithoutNull<DeviceMotionEventAcceleration> | null>(null);
-  const [orientation, setOrientation] =
-    useState<WithoutNull<DeviceMotionEventAcceleration> | null>(null);
+  const orientationOrigin = useRef<Orientation>(null);
+  const [orientation, setOrientation] = useState<Orientation>(null);
 
   useEffect(() => {
     handleDeviceMotionRequest(true);
 
     const deviceOrientationEventHandler = (event: DeviceOrientationEvent) => {
+      const x = event.beta ?? 0;
+      const y = event.gamma ?? 0;
+
+      if (!orientationOrigin.current) {
+        orientationOrigin.current = { x, y };
+        return;
+      }
+
       setOrientation({
-        z: event.alpha ?? 0,
-        x: event.beta ?? 0,
-        y: event.gamma ?? 0,
+        x: x - orientationOrigin.current.x,
+        y: y - orientationOrigin.current.y,
       });
     };
 
