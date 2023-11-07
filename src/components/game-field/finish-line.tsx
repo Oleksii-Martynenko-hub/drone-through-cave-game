713c893f6c4c289325f3c19e6c74276dddc76be5
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-11-07 17:21:21 +0200

    Fixed and refactor finish line

--- src/components/game-field/finish-line.tsx
+++ src/components/game-field/finish-line.tsx
@@ -7,30 +7,36 @@ type Props = {
 };
 
 const FinishLine = ({ dronePositionY, caveWallsData, windowHeight }: Props) => {
-  const wallsHeight = (caveWallsData.length - 1) * WALL_HEIGHT;
+  const generalWallsHeight = (caveWallsData.length - 1) * WALL_HEIGHT;
+  const passedWallsAmount = Math.floor(dronePositionY / WALL_HEIGHT);
+  const windowHeightWallsAmount = Math.ceil(windowHeight / WALL_HEIGHT);
 
-  const isLastWallDrawn =
-    windowHeight / WALL_HEIGHT + 1 + Math.floor(dronePositionY / WALL_HEIGHT) >=
-    caveWallsData.length;
+  const lastWall = caveWallsData.at(-1) || [0, 0];
 
-  const finishLineOffsetY = isLastWallDrawn
-    ? dronePositionY + windowHeight - wallsHeight
-    : 0;
+  const finishLineOffsetY = dronePositionY + windowHeight - generalWallsHeight;
 
-  const lastVisibleWallIndex =
-    windowHeight / WALL_HEIGHT + Math.floor(dronePositionY / WALL_HEIGHT);
+  const windowCenter = GAME_FIELD_MIN_WIDTH / 2;
+  const lineY = windowHeight - 1 - finishLineOffsetY;
 
-  const lastVisibleWall = caveWallsData.slice(
-    lastVisibleWallIndex,
-    lastVisibleWallIndex + 1,
-  )[0] || [0, 0];
+  const point1 = {
+    x: windowCenter + lastWall[0],
+    y: lineY,
+  };
 
-  return isLastWallDrawn ? (
+  const point2 = {
+    x: windowCenter + lastWall[1],
+    y: lineY,
+  };
+
+  const isDrawFinishLine =
+    windowHeightWallsAmount + passedWallsAmount >= caveWallsData.length;
+
+  return isDrawFinishLine ? (
     <line
-      x1={GAME_FIELD_MIN_WIDTH / 2 + lastVisibleWall[0] - 2}
-      x2={GAME_FIELD_MIN_WIDTH / 2 + lastVisibleWall[1] + 2}
-      y1={windowHeight - 1 - finishLineOffsetY}
-      y2={windowHeight - 1 - finishLineOffsetY}
+      x1={point1.x - 2}
+      y1={point1.y}
+      x2={point2.x + 2}
+      y2={point2.y}
       stroke="#4fccff"
       strokeWidth="2"
     />
