c05fd88d6876af6e4678192355eac8b43e276722
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-11-07 18:51:29 +0200

    Add decoration to finish

--- src/components/game-field/finish-line.tsx
+++ src/components/game-field/finish-line.tsx
@@ -1,5 +1,7 @@
 import { GAME_FIELD_MIN_WIDTH, WALL_HEIGHT } from 'src/constants';
 
+import Hangar from './hangar';
+
 type Props = {
   dronePositionY: number;
   caveWallsData: [number, number][];
@@ -31,15 +33,42 @@ const FinishLine = ({ dronePositionY, caveWallsData, windowHeight }: Props) => {
   const isDrawFinishLine =
     windowHeightWallsAmount + passedWallsAmount >= caveWallsData.length;
 
+  const lastSegmentWidth = point2.x - point1.x + 2;
+
   return isDrawFinishLine ? (
-    <line
-      x1={point1.x - 2}
-      y1={point1.y}
-      x2={point2.x + 2}
-      y2={point2.y}
-      stroke="#4fccff"
-      strokeWidth="2"
-    />
+    <>
+      <path
+        fill="#646464"
+        d={`M ${point1.x - 1},${lineY}
+           Q ${point1.x - 1},${lineY + lastSegmentWidth}
+            ${point1.x + lastSegmentWidth / 2 - 1},${lineY + lastSegmentWidth}
+           Q ${point1.x + lastSegmentWidth - 1},${lineY + lastSegmentWidth}
+           ${point1.x + lastSegmentWidth - 1},${lineY}
+           l 0 ${finishLineOffsetY} l ${-lastSegmentWidth - 1} 0 z`}
+      />
+
+      <text
+        x={point1.x + lastSegmentWidth / 2 - 43}
+        y={lineY + 40}
+        fontSize={24}
+        fontWeight={600}
+        stroke="black"
+        fill="#32c800"
+      >
+        FINISH
+      </text>
+
+      <Hangar x={point1.x + lastSegmentWidth / 2 - 0} y={lineY + 55} />
+
+      <line
+        x1={point1.x - 2}
+        y1={point1.y}
+        x2={point2.x + 2}
+        y2={point2.y}
+        stroke="#4fccff"
+        strokeWidth="2"
+      />
+    </>
   ) : null;
 };
 
