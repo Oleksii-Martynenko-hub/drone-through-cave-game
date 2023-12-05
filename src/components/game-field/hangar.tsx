c54536e49aa038d1d82b44251bcc62c484b30634
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-12-05 11:49:27 +0200

    Fixed svg attributes

--- src/components/game-field/hangar.tsx
+++ src/components/game-field/hangar.tsx
@@ -17,11 +17,11 @@ const Hangar = ({ x, y }: Props) => {
       fill="#000000"
       stroke="#000000"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {' '}
